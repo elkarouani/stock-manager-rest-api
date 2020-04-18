c420bf662204060ed397698eefc7449f7176fac7
KDragon <elkarouani@gmail.com>
2020-04-18 13:41:39 +0100

    Fix: deleting temporary env

--- drf-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/home/stock-manager-rest-api/drf-env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
