9a548a88ece8adcc23e96a698f6fde42e84a2d85
elkarouani <elkarouani@gmail.com>
2020-04-19 10:05:47 +0100

    Task: adding extra action for products model

--- core/stock/models.py
+++ core/stock/models.py
@@ -10,11 +10,12 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
 class Product(models.Model):
-    title = models.CharField(verbose_name="Product title", max_length=100)
+    title = models.CharField(verbose_name="Product title", max_length=100, unique=True)
     description = models.TextField(verbose_name="Product description")
     quantity = models.IntegerField(verbose_name="Product quantity")
     price = models.DecimalField(verbose_name="Product price", max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_run_out = models.BooleanField(verbose_name="Is the product run out of storage", default=False)
 
     def __str__(self):
         return self.title
--- core/stock/views.py
+++ core/stock/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Category, Product
 from .serializers import CategoriesSerializer, ProductsSerializer
 
@@ -9,3 +11,13 @@ class CategoriesViewSet(ModelViewSet):
 class ProductsViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductsSerializer
+
+    @action(detail=True, methods=['get'])
+    def run_out(self, request, pk=None):
+        product = self.get_object()
+        product.is_run_out = True
+        product.quantity = 0
+        product.save()
+        return Response({'status': "The product is run out of storage now !"})
+
+
