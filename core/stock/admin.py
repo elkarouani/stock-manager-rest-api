efd83d9767bc4631b51cd648954268be53c1670f
KDragon <elkarouani@gmail.com>
2020-04-24 14:54:48 +0000

    Task: adding integration for category model on django administration

--- core/stock/admin.py
+++ core/stock/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Category, Product
+from reversion.admin import VersionAdmin
 
-admin.site.register(Category)
 admin.site.register(Product)
+
+@admin.register(Category)
+class ClientModelAdmin(VersionAdmin):
+    pass
+
